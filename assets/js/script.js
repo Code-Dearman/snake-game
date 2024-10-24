9fee9fb847a2f7847e50ddcaedcb776b10be7eb9
Isaac Dearman <isaac.dearman@gmail.com>
2024-10-24 12:13:55 +0000

    add html boilerplate

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let blockSize = 25;
+let total_row = 17; //total row number
+let toal_col = 17; //total column number
+let board;
+let context;
+
+let snakeX = blockSize * 5;
+let snakeY = blockSize *5;
+
